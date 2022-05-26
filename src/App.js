aae9ca96d91b0e43cae36fce1a9120815b0d33a9
Luciana Zanetti <luciana.zeta@gmail.com>
2022-05-26 15:00:04 -0300

    finish

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import './App.css';
 import DropFileInput from './components/drop-file-input/DropFileInput';
 
 function App() {
+
+  const onFileChange = (files) => {
+    console.log(files)
+  }
+
   return (
     <div className="box">
       <h2 className='"header'>
           React Drop file input
       </h2>
-      <DropFileInput />
+      <DropFileInput
+        onFileChange={(files) => onFileChange(files)} />
     </div>
   );
 }
